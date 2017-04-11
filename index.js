8e2af005355824cbfe0b90d43b1795594112fee0
Uri Sarid <usarid@gmail.com>
2017-04-11 09:56:27 -0700

    Made the camera feed path and port configurable

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var httpProxy = require('http-proxy');
-var proxy = httpProxy.createProxyServer({
-});
+var proxy = httpProxy.createProxyServer({});
 
 proxy.on('proxyReq', function(proxyReq, req, res, options) {
   proxyReq.setHeader('Authorization', 'Basic YWRtaW46'); // base64 encoding of admin: (note trailing colon)
@@ -11,19 +10,34 @@ proxy.on('proxyRes', function(proxyRes, req, res, options) {
   proxyRes.headers['Access-Control-Allow-Origin'] = '*'; // CORS
 });
 
-CAMERA_DOMAIN_URL = 'http://192.168.86.147'; // Of the form http://192.168.86.147
-CAMERA_PATH = '/videostream.cgi';            // Of the form '/videostream.cgi'
-WEB_SERVER_PORT = 8080;
+DEFAULT_WEB_SERVER_PORT = 8080;
+
+if (process.argv.length < 3)
+{
+	console.log('Usage: node index.js http://url.to/camera.feed [port to serve on]');
+	console.log('Example: node index.js http://192.168.86.147/videostream.cgi 8090');
+	console.log('Default port: ' + DEFAULT_WEB_SERVER_PORT);
+	process.exit(0);
+}
+
+var cameraUrl = process.argv[2];
+var urlParts = /^(.*\:\/\/[^\/]*)(\/?.*$)/.exec(cameraUrl);
+cameraDomainUrl = urlParts[1];
+cameraPath = urlParts[2];
+console.log('Streaming camera video from base URL "' + cameraDomainUrl + 
+	'"" and path "' + cameraPath + '"');
+
+var webServerPort = (process.argv.length > 3) ? process.argv[3] : DEFAULT_WEB_SERVER_PORT;
 
 var app = express();
-app.get(CAMERA_PATH, function(req, res){
-  proxy.web(req, res, {target: CAMERA_DOMAIN_URL});  
+app.get(cameraPath, function(req, res){
+  proxy.web(req, res, {target: cameraDomainUrl});  
 });
 
 app.use(express.static('dashboard'))
 
-app.listen(WEB_SERVER_PORT, function () {
-    console.log('Server listening on: ' + WEB_SERVER_PORT);
+app.listen(webServerPort, function () {
+    console.log('Server listening on: ' + webServerPort);
   });
 
 // Helpful in debugging:
